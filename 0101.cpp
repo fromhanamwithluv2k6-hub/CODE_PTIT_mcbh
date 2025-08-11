05ddd8206ee85e87cbb21033c1282f205e971ebd
maychembienhoa <fromhanamwithluv2k6@gmail.com>
2025-08-12 05:59:50 +0700

    Add files via upload

---
+++ 0101.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+using namespace std;
+void solve()
+{
+	long long n;
+	cin >> n;
+	cout<<n*(n+1)/2<<endl;
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+
+}
+
