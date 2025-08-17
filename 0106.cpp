ba713d1f38c445e6192ff540437843de0de68aa2
maychembienhoa <fromhanamwithluv2k6@gmail.com>
2025-08-17 18:15:02 +0700

    0106.cpp

--- 0106.cpp
+++ 0106.cpp
@@ -1,8 +1,31 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
+bool check(string s)
+{
+	int l = s.size();
+	for (int i = 0; i < l / 2; i++)
+	{
+		int x = s[i] - '0';
+		int y = s[l - 1 - i] - '0';
+		if (x != y)
+			return 0;
+	}
+	return 1;
+}
 int main()
 {
-	string a;
-	cin >> a;
-	cout << a;
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		string s;
+		cin >> s;
+		if (check(s))
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+	}
+	return 0;
+
 }
