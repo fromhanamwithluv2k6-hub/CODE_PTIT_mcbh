dd80369908261e30733d5ca8cc9850fd583fd314
maychembienhoa <fromhanamwithluv2k6@gmail.com>
2025-08-19 07:28:30 +0700

    Add files via upload

---
+++ 0125.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+bool check(int n)
+{
+	if (n < 2)
+		return 0;
+	for (int i = 2; i <= sqrt(n); i++)
+	{
+		if (n % i == 0)
+			return 0;
+	}
+	return 1;
+}
+void swapp(int* a, int* b)
+{
+		int temp = *a;
+		*a = *b;
+		*b = temp;
+}
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	if (n > m)
+		swapp(&n, &m);
+	for (int i = n; i < m; i++)
+	{
+		if (check(i))
+			cout << i << " ";
+	}
+	return 0;
+}
