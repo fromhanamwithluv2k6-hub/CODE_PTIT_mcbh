53a919892065c937a63e6869413c267de8ecebec
maychembienhoa <fromhanamwithluv2k6@gmail.com>
2025-08-19 08:13:02 +0700

    Add files via upload

---
+++ 0127.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+bool check(int n)
+{
+	if (n < 2)
+		return 0;
+	for (int i = 2; i <= sqrt(n); i++)
+	{
+		if (n % i == 0)
+			return 0;
+	}
+	return 1;
+}
+void solve()
+{
+	int n;
+	cin >> n;
+	bool c = false;
+	for (int i = 2; i <= n / 2; i++)
+	{
+		if (check(i) && check(n - i))
+		{
+			cout << i << " " << n - i<<endl;
+			c = true;
+			break;
+		}
+	}
+	if (c == false)
+		cout << -1<<endl;
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		solve();
+	}
+}
